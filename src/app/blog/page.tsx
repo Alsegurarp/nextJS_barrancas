b4a9e18d5e2077b897c4f521a31ce96bec48f344
AlSegrarp <ALSEGURARP@GMAIL.COM>
2025-12-22 15:54:09 -0600

    blog inicio

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-900 dark:text-white">Blog</h1>
+    </div>
+  );
+}
