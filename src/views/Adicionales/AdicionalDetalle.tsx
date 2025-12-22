464b49cf2212992257e52ea82b8213b43171694f
AlSegrarp <ALSEGURARP@GMAIL.COM>
2025-12-22 12:56:42 -0600

    cambios adicionales cards

--- src/views/Adicionales/AdicionalDetalle.tsx
+++ src/views/Adicionales/AdicionalDetalle.tsx
@@ -46,7 +46,7 @@ function AdicionalDetalle() {
       {/* Wrapper container for max-width on large screens */}
       <div className='flex-1 flex flex-col md:flex-row md:mx-auto md:max-w-[1200px] md:w-full'>
         {/* Hero Image Section - Full width on mobile, 50% on desktop */}
-        <div className='relative w-full md:w-1/2 aspect-[3/4] min-[380px]:aspect-[5/5] xs:aspect-[4/3] min-[540px]:aspect-[4/3] sm:aspect-[6/4] md:aspect-auto overflow-hidden mb-6 md:mb-0 rounded-3xl'>
+        <div className='relative w-full md:w-1/2 aspect-[5/5] min-[380px]:aspect-[5/5] xs:aspect-[4/3] min-[540px]:aspect-[4/3] sm:aspect-[6/4] md:aspect-auto overflow-hidden mb-6 md:mb-0 rounded-3xl'>
               <button
                   onClick={() => router.push('/adicional')}
                   className='relative z-20 left-2.5 top-[70px] md:top-[90px] inline-block bg-white/20  dark:bg-primary-800/40 text-white dark:text-primary-200 px-3 py-1 rounded-full text-xs sm:text-sm font-semibold'
@@ -134,6 +134,19 @@ function AdicionalDetalle() {
                 <li>• Edad mínima: 12 años</li>
                 <li>• Condición física: Media</li>
                 <li>• Llevar bloqueador solar</li>
+                <li>• Se recomienda llevar zapatos cómodos, gorra o sombrero para el sol.</li>
+                
+                </ul>
+            </div>
+
+            <div className='bg-primary-400/40 backdrop-blur-md rounded-2xl p-5 min-w-[300px] sm:min-w-[350px]'>
+                <h3 className='text-gray-800 dark:text-white text-xs font-semibold uppercase mb-2'>
+                No incluye
+                </h3>
+                <ul className='text-sm text-gray-900 dark:text-white space-y-1'>
+                <li>• Alimentos ni bebidas no especificadas.</li>
+                <li>• Seguro turístico.</li>
+                <li>• Lo que no esté indicado en el apartado EL PRECIO `&ldquo;`INCLUYE`&ldquo;`.</li>
                 </ul>
             </div>
         </div>
